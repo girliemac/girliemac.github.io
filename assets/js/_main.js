2b0995ff6a9b9d1a668f3f77a75a81730af26ba4
Tomomi Imura <tomomi.i@gmail.com>
2014-02-14 13:44:45 -0800

    konami code

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -34,3 +34,13 @@ $(document).ready(function() {
     mainClass: 'mfp-fade'
   });
 });
+
+// Konami code	
+var cat = document.createElement('img');
+	cat.id = 'roombaCat';
+	cat.src = '/assets/images/roomba-cat.jpg';
+
+var k = new Konami(function(){
+	cat.classList.add('animate');
+	document.body.appendChild(cat);
+});
